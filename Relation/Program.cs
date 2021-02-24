0914bddc9ab1b0ed368abf7ac3f17eb24f7f1382
Fusionstak <Fusionstak@DESKTOP-2RC2PE1>
2021-02-23 19:07:14 -0800

    Example of dependency injection

---
+++ Relation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Relation
+{
+  public interface IShape
+    {
+       public  void area(int a , int b);
+    }
+    public class CalculateArea : IShape
+    {
+
+        public void area(int a, int b)
+        {
+            int ar = a * b;
+            Console.WriteLine($"area is : {ar}");
+        }
+    }
+    public class Prop
+    {
+        IShape _shape;
+        public Prop(IShape shape)
+        {
+            _shape = shape;
+        }
+        public void action()
+        {
+            _shape.area(3,4);
+        }
+    }
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            var Prop = new Prop(new CalculateArea());
+            Prop.action();
+        }
+    }
+   
+}
